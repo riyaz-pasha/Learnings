6acf00fdb6d97437d8a9cf2af17b75c5cbd98855
Riyaz <riyazpasha880@gmail.com>
2025-02-08 12:27:27 +0530

    [Riyaz][NextJs][nextjs-dashboard] Add. Layout and pages

---
+++ next-js/nextjs-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+// import Customer from "./customers/page";
+// import Invoices from "./invoices/page";
+
+export default function Page() {
+    return <p>Dashboard Page</p>
+    // return <div>
+    //     <div className={"min-h-screen"}>
+    //         <Customer />
+    //     </div>
+    //     <div className={"min-h-screen"}>
+    //         <Invoices />
+    //     </div>
+    // </div>
+};
