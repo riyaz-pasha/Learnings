f139f15d65fd748827c06f4a91aa6b6da9cb27f0
Riyaz <riyazpasha880@gmail.com>
2025-02-08 13:46:39 +0530

    [Riyaz][NextJs][nextjs-dashboard] Add. Dynaming rendering at component level using React Suspence

--- next-js/nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ next-js/nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,11 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData, fetchLatestInvoices, fetchRevenue } from '../../lib/data';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
+import { fetchCardData } from '../../lib/data';
 
 export default async function Page() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
     const {
         numberOfCustomers,
         numberOfInvoices,
@@ -30,8 +30,12 @@ export default async function Page() {
                 />
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                <RevenueChart revenue={revenue} />
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<RevenueChartSkeleton />}>
+                    <RevenueChart />
+                </Suspense>
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
