1fc1bc87ad3b5ac0bd42e5709e5a8265cceefae1
Riyaz <riyazpasha880@gmail.com>
2025-02-08 14:46:53 +0530

    [Riyaz][NextJs][nextjs-dashboard] Add. Pagination

--- next-js/nextjs-dashboard/app/dashboard/invoices/page.tsx
+++ next-js/nextjs-dashboard/app/dashboard/invoices/page.tsx
@@ -1,8 +1,9 @@
-import Pagination from '@/app/ui/invoices/pagination';
-import Search from '@/app/ui/search';
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { fetchInvoicesPages } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Pagination from '@/app/ui/invoices/pagination';
+import Table from '@/app/ui/invoices/table';
+import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 
@@ -13,6 +14,7 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -27,7 +29,7 @@ export default async function Page(props: {
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
     </div>
   );
--- next-js/nextjs-dashboard/app/ui/search.tsx
+++ next-js/nextjs-dashboard/app/ui/search.tsx
@@ -12,6 +12,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((value: string) => {
     console.log(value);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (value) {
       params.set('query', value);
     } else {
