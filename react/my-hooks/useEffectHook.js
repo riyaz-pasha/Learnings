797b5acc38c556a316f6701e88e1b0097dc13b28
Riyaz <riyazpasha880@gmail.com>
2025-03-20 15:23:09 +0530

    [Javascript] Add. React use effect custom hook

--- react/my-hooks/useEffectHook.js
+++ react/my-hooks/useEffectHook.js
@@ -2,6 +2,16 @@ function createCustomReact() {
 
     let state;
     let rerender = () => { };
+    let previousDependencies = [];
+    let cleanup;
+
+    function useEffect(callback, dependencies) {
+        const hasChanged = !previousDependencies || dependencies.some((dep, index) => dep !== previousDependencies[index]);
+        if (!hasChanged) return;
+        if (cleanup) cleanup();
+        cleanup = callback();
+        previousDependencies = dependencies;
+    }
 
     function useState(initialValue) {
         state = state ?? initialValue;
@@ -19,19 +29,25 @@ function createCustomReact() {
         component(); // Initial render
     }
 
-    return { render, useState }
+    return { render, useState, useEffect }
 }
 
-const { render, useState } = createCustomReact();
+const { render, useState, useEffect } = createCustomReact();
 
 function Counter() {
     const [count, setCount] = useState(0);
 
     console.log(`count: ${count}`);
 
+    useEffect(() => {
+        console.log("Effect ran!");
+
+        return () => console.log("Cleanup called before re ran");
+    }, [count])
+
     setTimeout(() => {
         setCount(count + 1); // This will trigger a re-run
-    }, 1000);
+    }, 2000);
 }
 
 render(Counter);
