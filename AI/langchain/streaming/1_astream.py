1b8dbab9a076b5e0d7906593acaa1f332e69ee9a
Riyaz <riyazpasha880@gmail.com>
2025-04-27 19:40:09 +0530

    [AI] langchain. Streaming print tokesn

--- AI/langchain/streaming/1_astream.py
+++ AI/langchain/streaming/1_astream.py
@@ -1,12 +1,16 @@
 import asyncio
 
 from langchain_ollama import ChatOllama
+from rich.pretty import pprint
 
 llm = ChatOllama(model="qwen2.5:latest")
+
+tokens = []
 
 
 async def stream_response(prompt: str):
     async for token in llm.astream(prompt):
+        tokens.append(token)
         print(token.content, end="|", flush=True)
 
 
@@ -16,3 +20,6 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+    pprint(tokens)
+    pprint("=" * 50)
+    pprint(tokens[0] + tokens[1] + tokens[2])
