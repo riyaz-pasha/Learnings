0897bf102456a76dae1eefab0d98a6a1dc77b408
Riyaz <riyazpasha880@gmail.com>
2025-07-26 19:30:57 +0530

    [Leetcode] binary-search. Rotation index

---
+++ leetcode-problems/binary-search/concepts/10-find-out-how-many-times-the-array-has-been-rotated.java
@@ -0,0 +1,63 @@
+class FindKRotation {
+
+    public static int findKRotation(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[high]) {
+                // Minimum is in the right half
+                low = mid + 1;
+            } else {
+                // Minimum is in the left half or at mid
+                high = mid;
+            }
+        }
+
+        return low; // low == index of minimum element
+    }
+
+    public static int findKRotation2(int[] arr) {
+        int low = 0, high = arr.length - 1;
+        int ans = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // search space is already sorted
+            // then arr[low] will always be
+            // the minimum in that search space:
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+                break;
+            }
+
+            // if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                if (arr[low] < ans) {
+                    index = low;
+                    ans = arr[low];
+                }
+
+                // Eliminate left half:
+                low = mid + 1;
+            } else { // if right part is sorted:
+
+                // keep the minimum:
+                if (arr[mid] < ans) {
+                    index = mid;
+                    ans = arr[mid];
+                }
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
+}
